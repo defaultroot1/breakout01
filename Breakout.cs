61893b4295e97627d71dd56070f4fccdec7d4ccf
defaultroot1 <mailniallc@gmail.com>
2023-07-18 18:52:31 +0100

    Small tweaks

--- Breakout.cs
+++ Breakout.cs
@@ -1,5 +1,6 @@
 ﻿using breakout01.Content.Helpers;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
